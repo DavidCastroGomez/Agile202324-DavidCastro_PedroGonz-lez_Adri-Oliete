548bab380ad0231e5fbb37fc31e701bd30c4f250
Pedro González Navarro <pedro.gonzalez@enti.cat>
2023-10-30 18:56:38 +0100

    Hero creation

---
+++ js/characters/hero.js
@@ -0,0 +1,11 @@
+class Hero extends Character
+{
+    constructor(){
+        //super.sprite = 
+        //super.healthSystem = 
+        //super.attackSystem = 
+        //super.movementSystem = 
+        //super.inputSystem = 
+        //super.collisionManager = 
+    }
+}
