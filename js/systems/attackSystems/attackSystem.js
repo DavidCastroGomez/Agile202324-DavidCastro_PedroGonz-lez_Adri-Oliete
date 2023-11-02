010a091ab95e446b0617a0b41dc61a26d1962c90
David <david.castro@enti.cat>
2023-11-02 13:20:17 +0100

    done

--- js/systems/attackSystems/attackSystem.js
+++ js/systems/attackSystems/attackSystem.js
@@ -1,3 +1,38 @@
 class AttackSystem{
     
+    constructor(_scene, _character){
+        this.scene = _scene
+        this.character = _character
+    }
+
+    AddEnemies(_enemies){
+        this.enemies = _enemies
+    }
+
+    Attack(direcion){
+
+        positionX = 0//TODO: Get character position_character.position
+        positiony = 0//TODO: Get character position_character.position
+
+
+        switch(direcion){
+            case 0:
+                positionX += 100; 
+
+            case 1:
+                positionY += 100; 
+
+            case 2:
+                positionX -= 100; 
+
+            case 3:
+                positionY -= 100; 
+
+        }
+
+        const colliderObject = new ColliderObject(scene, positionX, positionY, 5000);
+        _scene.physics.add.colldier(colliderObject, _enemies)
+
+    }
+
 }
