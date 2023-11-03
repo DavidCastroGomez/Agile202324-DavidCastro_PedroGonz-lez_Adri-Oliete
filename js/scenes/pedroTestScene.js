e9c881a585714f29cb4bf62dc37010f78366425c
Pedro González Navarro <pedro.gonzalez@enti.cat>
2023-11-03 17:55:20 +0100

    Now tileset png not being detected or something

--- js/scenes/pedroTestScene.js
+++ js/scenes/pedroTestScene.js
@@ -7,29 +7,71 @@ class PedroTestScene extends Phaser.Scene
 
     preload()
     { 
-        this.cameras.main.setBackgroundColor("255"); 
-        this.load.setPath('res/img');
-        this.load.image('bg_ground','TestGround.png');
-        this.load.spritesheet('link','TestLink.png');
+        this.cameras.main.setBackgroundColor("0");
+
+        this.load.setPath('res/img/tilesets');
+        //this.load.image('tiles', 'Alttp_Tileset.png');
+        this.load.image('tileset','Alttp_Lost_Woods.png');
+
+        this.load.setPath('res/maps');
+        //this.load.tilemapTiledJSON('Map', 'TestMap.json');
+        this.load.tilemapTiledJSON('level','Alttp_Lost_Woods.json');
     }
 
     create()
     {
-        this.bg_ground = this.add.tileSprite(0,0,config.width,config.height,'bg_ground').setOrigin(0);
-    }
+        //this.map = this.add.tilemap('Map');
+        //this.map.addTilesetImage('tiles');
+        //this.map.createLayer('ground_layer', 'tiles');
 
-    loadAnimations()
-    {
-        
-    }
-
-    loadPools()
-    {
-
-    }
-
-    update()
-    { //Actualiza whatever  
-
+        //this.map = this.add.tilemap('level');
+        //this.map.addTilesetImage('tileset');
+        //this.map.createLayer('lost_woods','tileset');
     }
 }
+
+/*    preload()
+    { //Carga assets en memoria
+        this.cameras.main.setBackgroundColor("112"); 
+        this.load.setPath('assets/sprites');
+        this.load.image('bg','bg_green_tile.png');
+
+        this.load.spritesheet('hero','hero.png',
+        {frameWidth:32,frameHeight:32});
+
+        this.load.setPath('assets/tilesets');
+        this.load.image('walls_tileset','tileset_walls.png');
+        this.load.image('moss_tileset','tileset_moss.png');
+        
+        this.load.setPath('assets/sounds');
+        
+        this.load.setPath('assets/maps');
+        this.load.tilemapTiledJSON('level1','level1.json');
+    }
+
+    create()
+    { //Pinta assets en pantalla
+        //Pintamos el fondo
+        this.add.tileSprite(0,0,gamePrefs.level1Width,gamePrefs.level1Height,'bg')
+        .setOrigin(0);
+
+        //Pintamos el nivel
+        //Cargo el JSON
+        this.map = this.add.tilemap('level1');
+        //Cargo los tilesets
+        this.map.addTilesetImage('walls_tileset');
+        this.map.addTilesetImage('moss_tileset');
+        //Pinto las CAPAS/LAYERS
+        this.map.createLayer('layer_walls','walls_tileset');
+        this.map.createLayer('layer_moss_up','moss_tileset');
+        this.map.createLayer('layer_moss_left','moss_tileset');
+        this.map.createLayer('layer_moss_right','moss_tileset');
+        this.map.createLayer('layer_moss_bottom','moss_tileset');
+
+        this.hero = this.physics.add.sprite(65,100,'hero');
+
+        this.cameras.main.startFollow(this.hero);
+        this.cameras.main.setBounds(0,0,
+            gamePrefs.level1Width,gamePrefs.level1Height);
+
+    }*/
